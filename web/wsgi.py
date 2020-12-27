f2b3fac716fe86612cb7e19d42f7d28ae654a67c
box-key <kei.box.fight@gmail.com>
2020-12-27 12:51:34 -0500

    added dockerfile; fixed input in wsig

--- web/wsgi.py
+++ web/wsgi.py
@@ -17,11 +17,16 @@ def index():
         language = request.form.get("language")
         input_text = request.form["input_text"]
         # send request to model server
-        # res = requests.post(MODEL_SERVER_ROUTE, data=input_text)
+        input_data = {
+            "lan": language,
+            "input_text": input_text,
+            "max_pred_len": len(input_text) + 5
+        }
+        res = requests.post(MODEL_SERVER_ROUTE, data=input_text)
         result = {}
-        # if res.status_code != 200:
-        #     result["prediction"] = "Bad input, try again"
-        #     return render_template('index.html', languages=LANGUAGES, res=result)
+        if res.status_code != 200:
+            result["prediction"] = "Bad input, try again"
+            return render_template('index.html', languages=LANGUAGES, res=result)
         # format output
         result["prediction"] = "?"
         return render_template('index.html', languages=LANGUAGES, res=result)
