86f8eef1b6eaa2ed4d4148cb7df9149a81969470
box-key <kei.box.fight@gmail.com>
2021-01-01 21:00:55 -0500

    fixed typo

--- web/wsgi_phonetisaurus.py
+++ web/wsgi_phonetisaurus.py
@@ -45,10 +45,10 @@ def index():
         beam_size = request.form.get("beam_size")
         input_text = request.form["input_text"]
         if len(input_text) == 0:
-            render_template('index.html',
-                            languages=LANGUAGES,
-                            beam_size=BEAM_SIZE,
-                            res=None)
+            return render_template('index.html',
+                                    languages=LANGUAGES,
+                                    beam_size=BEAM_SIZE,
+                                    res=None)
         # lower text and remove white space
         input_text = input_text.lower().replace(" ", "")
         result = {}
