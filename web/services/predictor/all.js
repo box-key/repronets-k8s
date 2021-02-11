d3d15c14f827809f84ed4c7be34d72aa9e8a22a6
box-key <kei.box.fight@gmail.com>
2021-02-10 22:18:13 -0500

    implemted async request for all option (test)

--- web/services/predictor/all.js
+++ web/services/predictor/all.js
@@ -1,11 +1,18 @@
 const axios = require('axios');
+const logger = require('../../loaders/logger')(module);
 
-module.exports = (input, language, beam) => {
-  let wow = {
+module.exports = async function(input, language, beam) {
+  params = {
     input: input,
     language: language,
-    beam: beam,
-    name: 'Your greed'
-  }
-  return wow
+    beam: beam
+  };
+  const psPrediction = axios.get('http://localhost:5001/predict', { params: params });
+  const tsPrediction = axios.get('http://localhost:5002/predict', { params: params });
+  const requests = [
+    psPrediction,
+    tsPrediction
+  ];
+  return axios.all(requests)
+    .catch(errors => { logger.error(errors) });
 };
