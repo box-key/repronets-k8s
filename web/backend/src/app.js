c45073e817d9a20492c1f9e3846f317e769718fe
box-key <underkey.box@outlook.com>
2022-01-15 16:16:11 +0000

    add cors to backend

--- web/backend/src/app.js
+++ web/backend/src/app.js
@@ -3,17 +3,28 @@ const logger = require('./loaders/logger')(module);
 const config = require('./config');
 const predict_route = require('./api/routes/predict');
 const bodyParser = require('body-parser')
+const cors = require("cors")
 
 async function startServer() {
   const app = express();
  
+  /* max payload limit */
   app.use(bodyParser.urlencoded({ extended: false, limit: '20mb' }));
   app.use(bodyParser.json({ limit: '20mb' }));
 
-  app.get('/', (req, res) => res.send('Hello World!'));
+  /* cors */
+  app.use(
+    cors({
+      origin: "*",
+      methods: ["GET", "POST"]
+    })
+  )
 
+  /* routes */
+  app.get('/', (req, res) => res.send('Hello World!'));
   app.use('/predict', predict_route());
 
+  /* start listening */
   let server = app.listen(config.port, () => {
     console.log(`
       #############################################
@@ -26,6 +37,7 @@ async function startServer() {
     process.exit(1);
   });
 
+  /* time out */
   server.setTimeout(config.timeout);
 
 }
